99bd4aaea99633292dc8f154b4552d8a1c248a2f
Sharma22053 <cschirag620.cs@gmail.com>
2025-11-18 16:07:11 +0530

    Building Roads Fast Input Implementation

---
+++ Graph_Algorithms/BuildingRoadsFastInput.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+ 
+public class BuildingRoadsFastInput {
+    static class DSU {
+        int[] parent, size;
+ 
+        DSU(int n) {
+            parent = new int[n + 1];
+            size = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+ 
+        int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]]; // path compression
+                x = parent[x];
+            }
+            return x;
+        }
+ 
+        void union(int a, int b) {
+            a = find(a);
+            b = find(b);
+            if (a == b) return;
+            if (size[a] < size[b]) {
+                parent[a] = b;
+                size[b] += size[a];
+            } else {
+                parent[b] = a;
+                size[a] += size[b];
+            }
+        }
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        int n = fs.nextInt();
+        int m = fs.nextInt();
+ 
+        DSU dsu = new DSU(n);
+ 
+        for (int i = 0; i < m; i++) {
+            dsu.union(fs.nextInt(), fs.nextInt());
+        }
+ 
+        ArrayList<Integer> reps = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (dsu.find(i) == i) reps.add(i);
+        }
+ 
+        System.out.println(reps.size() - 1);
+        for (int i = 1; i < reps.size(); i++) {
+            System.out.println(reps.get(0) + " " + reps.get(i));
+        }
+    }
+ 
+  
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+ 
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+ 
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') ;
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ') val = val * 10 + (c - '0');
+            return val * sign;
+        }
+    }
+}
